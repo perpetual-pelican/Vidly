26a8184f0764e79f8475f586f3d96a16ba18dff9
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-06-01 19:50:41 -0500

    finish testing middleware

--- tests/unit/middleware/auth.test.js
+++ tests/unit/middleware/auth.test.js
@@ -3,19 +3,47 @@ const { User } = require('../../../models/user');
 const auth = require('../../../middleware/auth');
 
 describe('auth middleware', () => {
-    it('should populate req.user with the payload of a valid JWT', () => {
-        const user = {
+    let user;
+    let req;
+    let res;
+    let next;
+
+    beforeEach(() => {
+        user = {
             _id: mongoose.Types.ObjectId().toHexString(),
-            isAdmin: true
+            isAdmin: false
         };
         const token = new User(user).generateAuthToken();
-        const req = {
-            header: jest.fn().mockReturnValue(token)
-        };
-        const res = {};
-        const next = jest.fn();
+        req = { header: jest.fn().mockReturnValue(token) };
+        res = { status: jest.fn().mockReturnValue({ send: jest.fn() }) };
+        next = jest.fn();
+    });
+
+    it('should set status to 401 if no token provided', async () => {
+        req.header = req.header.mockReturnValue('');
+
+        auth(req, res, next);
+
+        expect(res.status).toHaveBeenCalledWith(401);
+    });
+
+    it('should set status to 400 if token is invalid', async () => {
+        req.header = req.header.mockReturnValue('invalid');
+
+        auth(req, res, next);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+    });
+
+    it('should populate req.user with user object if token is valid', () => {
         auth(req, res, next);
 
         expect(req.user).toMatchObject(user);
+    });
+
+    it('should call next if token is valid', () => {
+        auth(req, res, next);
+
+        expect(next).toHaveBeenCalled();
     });
 });
