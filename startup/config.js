ea13ab23c27294aae853851d25f17493c4c4c7b8
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-06-15 02:04:14 -0500

    ensure mongodb connection string is cross-platform

--- startup/config.js
+++ startup/config.js
@@ -1,10 +1,11 @@
 const config = require('config');
-const hostname = require('os').hostname();
+const os = require('os');
 
 if (!config.get('jwtPrivateKey'))
     throw new Error('FATAL ERROR: vidly_jwtPrivateKey is not defined.');
 
-const dbString =
-    `mongodb://${hostname}:27017,${hostname}:27018,${hostname}:27019/vidly`;
+const host = os.type() === 'Windows_NT' ? os.hostname() : 'localhost';
+const dbString = `mongodb://${host}:27017,${host}:27018,${host}:27019/vidly`;
+const env = process.env.NODE_ENV || 'development';
 
-module.exports = { db: `${dbString}_${process.env.NODE_ENV || 'development'}` };
+module.exports = { db: `${dbString}_${env}` };
