81217cf669ec58879c6e10786379b8be148f6400
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-09-09 16:14:34 -0500

    allow env vars for db connection

--- backend/startup/config.js
+++ backend/startup/config.js
@@ -1,10 +1,21 @@
 const os = require('os');
 
+// need private key for jwt authentication
 if (!process.env.vidly_jwtPrivateKey)
   throw new Error('FATAL ERROR: vidly_jwtPrivateKey is not defined.');
 
-const host = os.type() === 'Windows_NT' ? os.hostname() : 'localhost';
-const dbString = `mongodb://${host}:27017,${host}:27018,${host}:27019?replicaSet=rs`;
+let dbString;
+if (process.env.vidly_hosts) {
+  // using custom replicaSet, use provided hosts
+  // also use provided rs name or "vidly_rs" as default
+  const hosts = process.env.vidly_hosts;
+  const rs = process.env.vidly_rs || 'vidly_rs';
+  dbString = `mongodb://${hosts}/?replicaSet=${rs}`;
+} else {
+  // using run-rs for replicaSet, just use defaults for OS
+  const host = os.type() === 'Windows_NT' ? os.hostname() : 'localhost';
+  dbString = `mongodb://${host}:27017,${host}:27018,${host}:27019/?replicaSet=rs`;
+}
 const env = process.env.NODE_ENV || 'development';
 
 module.exports = {
