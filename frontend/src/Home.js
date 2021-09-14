30e56305ca6308ba7615c0733780ccbc48e595c4
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-09-14 01:44:57 -0500

    add signup/login/logout functionality to home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Genres from './Genres';
 import Movies from './Movies';
 
 const Home = () => {
+  const token = sessionStorage.getItem('token');
+
   return (
     <>
       <h1>Vidly</h1>
+      {token && (
+        <div>
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              sessionStorage.removeItem('token');
+              window.location.reload(false);
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      )}
+      {!token && (
+        <>
+          <div>
+            <Link to="/login">Login</Link>
+          </div>
+          <div>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </>
+      )}
       <Genres />
       <Movies />
     </>
