87e300464b0d3161982753b727814520c091ed36
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-09-15 20:06:20 -0500

    remove unnecessary router/paths from App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Home';
-import Login from './Login';
-import Signup from './Signup';
-import Genres from './Genres.js';
-import Movies from './Movies.js';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/genres" component={Genres} />
-        <Route path="/movies" component={Movies} />
-      </div>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
