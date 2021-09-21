7be9377bd339c902e79c2694b0e6ea47b7bb7e33
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-09-20 22:45:28 -0500

    adding validation logic to signup form

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,25 +1,49 @@
 import React, { useState } from 'react';
-import { TextField } from '@mui/material';
+import { TextField, Alert } from '@mui/material';
 import { register } from './util/request';
 import AuthDialog from './components/AuthDialog';
+import { user as bounds } from './util/modelConstraints';
+const {
+  validateName,
+  validateEmail,
+  validatePassword,
+  validatePasswordMatch,
+} = require('./util/userValidators');
+
+const errorMessages = {
+  name: `Name must be between ${bounds.name.min} and ${bounds.name.max} characters`,
+  email: `Please enter a valid email`,
+  password: `Password must be between ${bounds.password.min} and ${bounds.password.max} characters and include 1 lowercase, 1 uppercase, 1 number, and 1 symbol`,
+  confirmPassword: 'Passwords must match',
+};
 
 const Signup = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [fields, setFields] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [errors, setErrors] = useState({});
+  const [formIsValid, setFormIsValid] = useState(false);
 
   const submit = async () => {
-    if (password !== confirmPassword) {
-      alert('Passwords do not match');
-      return;
-    }
-    const body = { name, email, password };
+    if (!formIsValid) return false;
+    const body = {
+      name: fields.name,
+      email: fields.email,
+      password: fields.password,
+    };
     try {
       const data = await register(body);
       if (typeof data === 'string') {
-        alert(data);
+        setErrors((errors) => ({ ...errors, form: data }));
         return false;
+      } else {
+        setErrors((errors) => {
+          delete errors.form;
+          return errors;
+        });
       }
     } catch (e) {
       console.error(e);
@@ -28,10 +52,40 @@ const Signup = () => {
   };
 
   const resetForm = () => {
-    setName('');
-    setEmail('');
-    setPassword('');
-    setConfirmPassword('');
+    setFields({ name: '', email: '', password: '', confirmPassword: '' });
+    setErrors({});
+    setFormIsValid(false);
+  };
+
+  const validate = (field, validator, value, value2) => {
+    const { error } = validator(value, value2);
+    if (error) {
+      setErrors((errors) => {
+        errors[field] = errorMessages[field];
+        return errors;
+      });
+      setFormIsValid(false);
+    } else if (errors[field]) {
+      setErrors((errors) => {
+        delete errors[field];
+        validateForm(errors);
+        return errors;
+      });
+    }
+  };
+
+  const validateForm = (errors) => {
+    const filledFieldCount = Object.keys(fields).length - 4;
+    const isFilled = filledFieldCount >= 3 ? true : false;
+    const hasError =
+      errors.name || errors.email || errors.password || errors.confirmPassword;
+    if (!isFilled || hasError) {
+      console.log(filledFieldCount);
+      console.log(errors);
+      setFormIsValid(false);
+    } else {
+      setFormIsValid(true);
+    }
   };
 
   return (
@@ -40,43 +94,106 @@ const Signup = () => {
       text="Enter your name, email address, and password to create an account."
       submit={submit}
       resetForm={resetForm}
+      submitDisabled={!formIsValid}
     >
+      {errors.form ? <Alert severity="warning">{errors.form}</Alert> : null}
+      {fields.nameIsFilled && errors.name ? (
+        <Alert severity="warning">{errors.name}</Alert>
+      ) : null}
       <TextField
         autoFocus
+        required
         fullWidth
         margin="dense"
         type="text"
         id="name"
         label="Name"
-        value={name}
-        onChange={(event) => setName(event.target.value)}
+        value={fields.name}
+        onChange={(event) => {
+          setFields((fields) => ({ ...fields, name: event.target.value }));
+          validate('name', validateName, event.target.value);
+        }}
+        onBlur={(event) => {
+          setFields((fields) => ({ ...fields, nameIsFilled: true }));
+          validate('name', validateName, event.target.value);
+        }}
       />
+      {fields.emailIsFilled && errors.email ? (
+        <Alert severity="warning">{errors.email}</Alert>
+      ) : null}
       <TextField
+        required
         fullWidth
         margin="dense"
         type="email"
         id="email"
         label="Email Address"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
+        value={fields.email}
+        onChange={(event) => {
+          setFields((fields) => ({ ...fields, email: event.target.value }));
+          validate('email', validateEmail, event.target.value);
+        }}
+        onBlur={(event) => {
+          setFields((fields) => ({ ...fields, emailIsFilled: true }));
+          validate('email', validateEmail, event.target.value);
+        }}
       />
+      {fields.passwordIsFilled && errors.password ? (
+        <Alert severity="warning">{errors.password}</Alert>
+      ) : null}
       <TextField
+        required
         fullWidth
         margin="dense"
         type="password"
         id="password"
         label="Password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
+        value={fields.password}
+        onChange={(event) => {
+          setFields((fields) => ({ ...fields, password: event.target.value }));
+          validate('password', validatePassword, event.target.value);
+          validate(
+            'confirmPassword',
+            validatePasswordMatch,
+            event.target.value,
+            fields.confirmPassword
+          );
+        }}
+        onBlur={(event) => {
+          setFields((fields) => ({ ...fields, passwordIsFilled: true }));
+          validate('password', validatePassword, event.target.value);
+          validate(
+            'confirmPassword',
+            validatePasswordMatch,
+            event.target.value,
+            fields.confirmPassword
+          );
+        }}
       />
+      {fields.confirmPasswordIsFilled && errors.confirmPassword ? (
+        <Alert severity="warning">{errors.confirmPassword}</Alert>
+      ) : null}
       <TextField
+        required
         fullWidth
         margin="dense"
         type="password"
         id="confirm password"
         label="Confirm Password"
-        value={confirmPassword}
-        onChange={(event) => setConfirmPassword(event.target.value)}
+        value={fields.confirmPassword}
+        onChange={(event) => {
+          setFields((fields) => ({
+            ...fields,
+            confirmPassword: event.target.value,
+            confirmPasswordIsFilled: true,
+          }));
+          validate(
+            'confirmPassword',
+            validatePasswordMatch,
+            fields.password,
+            event.target.value
+          );
+        }}
       />
     </AuthDialog>
   );
---
+++ frontend/src/util/userValidators.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity').default;
+const tlds = require('@sideway/address/lib/tlds');
+const { user } = require('./modelConstraints');
+
+const nameSchema = Joi.string()
+  .min(user.name.min)
+  .max(user.name.max)
+  .required();
+
+const emailSchema = Joi.string()
+  .min(user.email.min)
+  .max(user.email.max)
+  .email({
+    tlds: {
+      allow: tlds,
+    },
+  })
+  .required();
+
+const passwordSchema = passwordComplexity({
+  min: user.password.min,
+  max: user.password.max,
+  lowerCase: 1,
+  upperCase: 1,
+  numeric: 1,
+  symbol: 1,
+  requirementCount: 4,
+}).required();
+
+module.exports = {
+  validateName(name) {
+    return nameSchema.validate(name);
+  },
+  validateEmail(email) {
+    return emailSchema.validate(email);
+  },
+  validatePassword(password) {
+    return passwordSchema.validate(password);
+  },
+  validatePasswordMatch(password, confirm) {
+    if (password !== confirm) {
+      return { error: true };
+    } else {
+      return {};
+    }
+  },
+};
