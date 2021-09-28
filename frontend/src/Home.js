cbcfd0b0e23399278f8e75935d8ce1310d9f5b42
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-09-27 19:36:23 -0500

    use tabs for accessing api resources

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,20 +1,57 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Tabs, Tab, Typography, Button } from '@mui/material';
 import Login from './Login';
 import Signup from './Signup';
 import Genres from './Genres';
 import Movies from './Movies';
 
+const TabPanel = (props) => {
+  const { children, value, index } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+    >
+      {value === index && children}
+    </div>
+  );
+};
+
 const Home = () => {
   const token = sessionStorage.getItem('token');
+  const [currentTab, setCurrentTab] = useState(0);
 
   return (
     <>
       <AppBar>
         <Toolbar>
-          <Typography variant="h4" sx={{ flexGrow: 1 }}>
-            Vidly
-          </Typography>
+          <Typography variant="h4">Vidly</Typography>
+          <Tabs
+            value={currentTab}
+            onChange={(event, newTab) => {
+              setCurrentTab(newTab);
+            }}
+            aria-label="tabs"
+            textColor="secondary"
+            indicatorColor="secondary"
+            centered
+            sx={{ flexGrow: 1 }}
+          >
+            <Tab
+              label="Genres"
+              value={0}
+              id="tab-0"
+              aria-controls="tabpanel-0"
+            />
+            <Tab
+              label="Movies"
+              value={1}
+              id="tab-1"
+              aria-controls="tabpanel-1"
+            />
+          </Tabs>
           {token && (
             <Button
               color="inherit"
@@ -35,8 +72,12 @@ const Home = () => {
         </Toolbar>
       </AppBar>
       <Toolbar />
-      <Genres />
-      <Movies />
+      <TabPanel value={currentTab} index={0}>
+        <Genres />
+      </TabPanel>
+      <TabPanel value={currentTab} index={1}>
+        <Movies />
+      </TabPanel>
     </>
   );
 };
