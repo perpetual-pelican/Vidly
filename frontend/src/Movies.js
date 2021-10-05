8c9fd1afbedd147c76422a3d1ffa646b6148bacf
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-10-05 16:17:50 -0500

    add toggles for list/table and form

--- frontend/src/Movies.js
+++ frontend/src/Movies.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { Grid, FormGroup, FormControlLabel, Switch } from '@mui/material';
 import MovieList from './components/MovieList';
 import MovieTable from './components/MovieTable';
 import MovieForm from './components/MovieForm';
 
 const Movies = (props) => {
   const { movies, setMovies, genres } = props;
+  const [showTable, setShowTable] = useState(false);
+  const [showForm, setShowForm] = useState(false);
   const token = sessionStorage.getItem('token');
 
   return (
@@ -13,14 +15,47 @@ const Movies = (props) => {
       <Grid
         container
         item
-        lg={token ? 8 : 12}
+        lg={showForm ? 8 : 12}
         justifyContent="center"
-        mt={5}
         mb={5}
       >
-        {token ? <MovieTable movies={movies} /> : <MovieList movies={movies} />}
+        {token && (
+          <Grid container>
+            <FormGroup sx={{ flexGrow: 1 }}>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={showTable}
+                    onChange={() => {
+                      setShowTable((show) => !show);
+                    }}
+                  />
+                }
+                label="Show Table"
+              />
+            </FormGroup>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={showForm}
+                    onChange={() => {
+                      setShowForm((show) => !show);
+                    }}
+                  />
+                }
+                label="Show Form"
+              />
+            </FormGroup>
+          </Grid>
+        )}
+        {showTable ? (
+          <MovieTable movies={movies} />
+        ) : (
+          <MovieList movies={movies} />
+        )}
       </Grid>
-      {token && (
+      {showForm && (
         <Grid container item lg={4} justifyContent="center" mt={2}>
           <MovieForm movies={movies} setMovies={setMovies} genres={genres} />
         </Grid>
