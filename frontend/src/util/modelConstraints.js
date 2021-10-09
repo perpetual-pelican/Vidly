1a9d2eeedebccd23af54714de0c6992b5d37679c
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2021-10-09 14:33:26 -0500

    update modelConstraints to match backend

--- frontend/src/util/modelConstraints.js
+++ frontend/src/util/modelConstraints.js
@@ -8,11 +8,11 @@ exports.customer = {
   phone: { min: 5, max: 32 },
 };
 
-exports.genre = { name: { min: 3, max: 128 } };
+exports.genre = { name: { min: 3, max: 64 } };
 
 exports.movie = {
-  title: { min: 3, max: 128 },
+  title: { min: 3, max: 64 },
   dailyRentalRate: { min: 0, max: 20 },
   numberInStock: { min: 0, max: 1000 },
-  genres: { min: 1, max: 10 },
+  genres: { min: 1, max: 5 },
 };
