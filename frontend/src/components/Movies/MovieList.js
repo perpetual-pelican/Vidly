7b675129c83fb7e89d5473f445cb8d8a83ced516
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2024-02-17 22:38:04 -0600

    Fix Movie List Grid Layout

--- frontend/src/components/Movies/MovieList.js
+++ frontend/src/components/Movies/MovieList.js
@@ -9,34 +9,46 @@ const MovieList = ({ movies, shift }) => {
 
   const colShift = shift ? -1 : 0;
   let numColumns = 1;
-  if (isXlUp) numColumns = 3 + colShift;
+  if (movies.length < 3) numColumns = movies.length;
+  else if (isXlUp) numColumns = 3 + colShift;
   else if (isLgUp) numColumns = 2 + colShift;
 
-  const colSize = Math.ceil(movies.length / numColumns);
+  let colSize = Math.ceil(movies.length / numColumns);
   const cols = [];
+  // split movies into columns for UI layout
   for (let i = 0; i < numColumns; i++) {
     cols[i] = movies.slice(colSize * i, (i + 1) * colSize);
+    if (i === 0 && numColumns === 3 && movies.length % numColumns === 1)
+      colSize--;
   }
+  // alternate way of splitting movies
+  // for (let i = 0; i < movies.length; i++) {
+  //   if (i < numColumns) cols.push([movies[i]]);
+  //   else cols[i % numColumns].push(movies[i]);
+  // }
 
   return (
     <>
-      {cols.map((col) => (
-        <Grid
-          key={col[0]._id}
-          container
-          item
-          sm={12 / numColumns}
-          justifyContent="center"
-        >
-          <List>
-            {col.map((movie) => (
-              <ListItem key={movie._id}>
-                <Chip label={movie.title} />
-              </ListItem>
-            ))}
-          </List>
-        </Grid>
-      ))}
+      {cols.map(
+        (col) =>
+          col.length > 0 && (
+            <Grid
+              key={col[0]._id}
+              container
+              item
+              sm={12 / numColumns}
+              justifyContent="center"
+            >
+              <List>
+                {col.map((movie) => (
+                  <ListItem key={movie._id}>
+                    <Chip label={movie.title} />
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
+          )
+      )}
     </>
   );
 };
